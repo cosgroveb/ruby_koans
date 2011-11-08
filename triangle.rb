76e88bd69e1d43e8f7822693a74379770bf19be7
Brian Cosgrove <cosgroveb@gmail.com>
2011-11-07 18:15:40 -0600

    Completes part one of the triangle koans

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  unique_sides = [a,b,c].uniq.count
+  if unique_sides == 1
+    :equilateral
+  elsif unique_sides == 2
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
