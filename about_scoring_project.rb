3f03a66939f802120c35b86639e3c36948c2ce95
Brian Cosgrove <cosgroveb@gmail.com>
2011-11-09 19:48:16 -0600

    Completes scoring project. Not the most elegant solution.

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,7 +30,25 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  score = 0
+
+  ones = dice.find_all{|item| item == 1}.count
+  dice.delete(1)
+  score += 1000 if ones >= 3
+  ones -= 3 if ones >= 3
+  score += ones * 100
+
+  fives = dice.find_all{|item| item == 5}.count
+  dice.delete(5)
+  score += 500 if fives >= 3
+  fives -= 3 if fives >= 3
+  score += fives * 50
+
+  remaining = dice.uniq
+  remaining.each do |roll|
+    score += roll*100 if dice.count(roll) >= 3
+  end
+  score
 end
 
 class AboutScoringProject < EdgeCase::Koan
